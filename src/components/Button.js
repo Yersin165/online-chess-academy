7708a3cc27075535dfd230d91290fd673d9f1471
Yersin <yersin.nurgeldiuly@narxoz.kz>
2026-03-16 12:51:42 +0500

    Update Button.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Button({ text, onClick }) {
+function Button({ text, onClick }) // destructuring: props.text -> just text {
   return (
     <button className="custom-button" onClick={onClick}>
       {text}
